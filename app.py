c7037737a11cc5d33ee9b1956e463e92f93a6278
edin93 <il_matador@hotmail.fr>
2020-04-28 17:26:14 +0100

    fix typo

--- app.py
+++ app.py
@@ -44,7 +44,7 @@ def create_task():
     if not request.json or not 'title' in request.json:
         abort(400)
     task = {
-        'id': len(tasks),
+        'id': tasks[-1]['id'] + 1,
         'title': request.json['title'],
         'description': request.json.get('description', ""),
         'done': False
@@ -53,7 +53,7 @@ def create_task():
     return jsonify({'task': task}), 201
 
 
-@app.route('/todo/api/v1.0/tasks/<int: task_id}', methods=['PUT'])
+@app.route('/todo/api/v1.0/tasks/<int:task_id>', methods=['PUT'])
 def update_task(task_id):
     task = [task for task in tasks if task['id'] == task_id]
     if len(task) == 0:
